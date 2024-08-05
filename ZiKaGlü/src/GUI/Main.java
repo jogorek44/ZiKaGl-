08c0d895d40bf6b58655da8028b334f44c1b89c7
Leon <s_tzikas23@stud.hwr-berlin.de>
2024-08-05 14:45:24 +0200

    bissel main

--- ZiKaGlü/src/GUI/Main.java
+++ ZiKaGlü/src/GUI/Main.java
@@ -1,20 +1,47 @@
 package GUI;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Menu p1 = new Menu();
-		p1.setVisible(true);
-		System.out.print("p1");
-		Menu p2 = new Menu();
-		p2.setVisible(true);
-		System.out.print("p2");
-		Menu p3 = new Menu();
-		p3.setVisible(true);
-		System.out.print("p3");
-		Menu p4 = new Menu();
-		p4.setVisible(true);
-		System.out.print("p4");
+		init();
 	}
 
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	int screenWidth = screenSize.width;
+	int screenHeight = screenSize.height;
+
+	public static void init() {
+		Menu p1 = new Menu();
+		p1.setVisible(true);
+		System.out.print("p1 ");
+		setPos(p1, 1);
+		Menu p2 = new Menu();
+		p2.setVisible(true);
+		System.out.print("p2 ");
+		setPos(p2, 2);
+		Menu p3 = new Menu();
+		p3.setVisible(true);
+		System.out.print("p3 ");
+		setPos(p3, 3);
+		Menu p4 = new Menu();
+		p4.setVisible(true);
+		System.out.print("p4 ");
+		setPos(p4, 4);
+	}
+
+	public static void setPos(Menu temp, int pos) {
+		switch (pos) {
+		case 1:
+			temp.frame.setLocation(0, 0);
+		case 2:
+			temp.frame.setLocation(1620, 0);
+		case 3:
+			temp.frame.setLocation(0, 880);
+		case 4:
+			temp.frame.setLocation(1620, 880);
+		}
+	}
 }
