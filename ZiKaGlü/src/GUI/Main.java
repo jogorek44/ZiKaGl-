1be12e41fd8eb513c19b03b06cf9307f7203bda7
Leon <s_tzikas23@stud.hwr-berlin.de>
2024-08-05 14:19:43 +0200

    Create Main.java

---
+++ ZiKaGlü/src/GUI/Main.java
@@ -0,0 +1,20 @@
+package GUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Menu p1 = new Menu();
+		p1.setVisible(true);
+		System.out.print("p1");
+		Menu p2 = new Menu();
+		p2.setVisible(true);
+		System.out.print("p2");
+		Menu p3 = new Menu();
+		p3.setVisible(true);
+		System.out.print("p3");
+		Menu p4 = new Menu();
+		p4.setVisible(true);
+		System.out.print("p4");
+	}
+
+}
